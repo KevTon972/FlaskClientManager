f58bcdacc0c5752cdca3787d0f5a959953d074be
kevin <kevin.clerima@outlook.fr>
2024-01-08 10:20:51 +0100

    added changes

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, logout_user
+from secret_key import SECRET_KEY
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/flaskdb'
+app.config['SECRET_KEY'] = SECRET_KEY
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 with app.app_context():
     db.create_all()
@@ -24,6 +30,17 @@ class Utilisateur(db.Model):
         self.username = username
         self.email = email
         self.password = password
+    
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        return self.id
+
+
+@login_manager.user_loader
+def loader_user(user_id):
+    return Utilisateur.query.get(user_id)
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -33,6 +50,7 @@ def index():
 
 @app.route("/register/", methods=['GET', 'POST'])
 def register():
+    # register the user with form informations
     if request.method == 'POST':
         username = request.form.get('username')
         email = request.form.get('email')
@@ -40,34 +58,43 @@ def register():
         confirm_password = request.form.get('confirm_password')
 
         if password == confirm_password:
-            new_user = Utilisateur(username=username, email=email, password=generate_password_hash(password, method='pbkdf2:sha1', salt_length=15))
-            print(new_user.password)
+            new_user = Utilisateur(username=username,
+                                   email=email,
+                                   password=generate_password_hash(password, method='pbkdf2:sha1', salt_length=15))
 
             try:
-
                 db.session.add(new_user)
                 db.session.commit()
 
                 return redirect(url_for('login'))
         
             except:
-                print("Erreur dans l'enregistrement des données")
-
+                flash("Error with datas", 'error')
                 return render_template('register.html')
         else:
-            # ajouter affichage message d'erreur avec flash
-            print('too bad')
+            # display error message 
+            flash('Problem with the Password')
             return render_template('register.html')
-
-        
 
     return render_template('register.html')
 
 
 @app.route("/login/", methods=['GET', 'POST'])
 def login():
-    # ajouter logique pour connecter l'utilisateur
+    # check if informations are correct and connect the user
+    if request.method == 'POST':
+        user = Utilisateur.query.filter_by(username=request.form.get('username')).first()
+        
+        if check_password_hash(user.password, request.form.get('password')) and user.username == request.form.get('username'):
+            login_user(user,remember=True)
+            flash('Successful Login', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash("Your username or your password aren't correct", 'error')
+            return render_template('login.html')
+        
     return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
